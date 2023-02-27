a6d2668d01c510ba3c3599a7959679e4c727550b
Ahmed Rahi <arahi@fordham.edu>
2023-02-27 10:42:49 -0500

    tried to fetch api but unsucessful

--- public/api.js
+++ public/api.js
@@ -1,1 +1,10 @@
 const URL = "https://project-2-nfb3vpy9l-arahi7860.vercel.app/api/demons";
+
+const getDemons = async () => {
+  const result = await fetch(URL);
+  const json = await result.json();
+
+  return json[0];
+};
+
+getDemons();
